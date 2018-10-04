c7dad3a892912b97eee2b698a1df0e9925e3755e
Bon Hyeok Koo <k01032633280@gmail.com>
2018-10-04 10:48:50 +0900

    tt

--- week_01/src/week_03/MyFrame.java
+++ week_01/src/week_03/MyFrame.java
@@ -15,7 +15,7 @@ public class MyFrame extends JFrame  implements KeyListener,ActionListener{
 	JLabel t1;  
 	JPanel p1,p2,p3;
 	
-	//String key[]= {"0","1","2","3","4","5","6","7","8","9","+","q"};
+	String key[]= {"0","1","2","3","4","5","6","7","8","9","+","..etc"};
 	
 	int NumButton = 16;
 	Calculator cal;
