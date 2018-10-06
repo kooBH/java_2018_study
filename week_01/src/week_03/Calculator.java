a5caa83f4f57c804aca633a2cce8a98126e3c50d
Bon Hyeok Koo <k01032633280@gmail.com>
2018-10-06 12:06:22 +0900

    갈길이 멀다

--- week_01/src/week_03/Calculator.java
+++ week_01/src/week_03/Calculator.java
@@ -1,54 +1,128 @@
 package week_03;
 import java.text.DecimalFormat;
 public class Calculator {
+	// 현재 입력중인 숫자
 	double input;
 	double output;
-	double mid;
-	char operator;
+	// 현재 수행 예정인 연산자
+	// 값이 0이면 연산자 입력을 아직 받지 않는 상태
+	char op;
+	// 계산을 하는 연산자들, 입력 확인용 
+	char[] listOp = {'+','-','*','/'};
+	/* 가장 최근 입력의 열거형 - C의 열거형과 유사
+	 enum 은 개발자의 편의를 돕기위해 각각의 정수에 매핑되는 이름을 부여한것
+	 0부터 1씩 증가하며 OP느 0, NUM은 1의 값을 가지지만
+	 State.OP, State.NUM이런식으로 직관적인 사용이 가능하다
+	*/
+	enum State {OP, NUM, DOT}
+	State state;
+	//UI에 표시하기 위해 보낼 출력 스트링
 	String print;
+	//출력 포맷
 	DecimalFormat formatter;
+	
 	Calculator(){
 		/*
 		 * 일반적으로 실수형을 표현할때 길이가 어느정도 길어지면 a.bxc * 10E
 		 * 꼴로 표현된다. 계산기는 그렇게 표현하지 않을 것이기 때문에
 		 * 문자열의 표현방식인 포맷을 변경해서 출력한다
+		 * 숫자 3개 당 , 표시를 하고 소숫점 
 		 * */
+		state = State.NUM;
 		formatter = new DecimalFormat("#,###.##########");
 		output = 0;
+		input = 0;
 		print = "0";
+		op = 0;
 	}
 	/* 계산기 입력을 받았을 때의 처리 
-	 * 
-	 * 1. 처음 숫자입력
-	 * 2. =이 아닌 연산자입력
-	 * 3. =의 입력
-	 * 4. 연산자 이후 숫자입력
-	 * etc..
+	 * 입력의 종류는 3가지
+	 * 1. 계산하는 연산자(*,/,+,/)
+	 * 2. 기타 연산자(Backspace, clear, = )
+	 * 3. 숫자입력
 	 * */
 	public void input(String v) {
+		int i;
+		//현재 Op
+		char curOp=0;
+		boolean isCalOp=false;
 		System.out.println("Calculator input : " + v);
-		if(v.equals("+")) {}
-		else if(v.equals("+")) {operator = '+';}
-		else if(v.equals("-")) {operator = '-';}
-		else if(v.equals("*")) {operator = '*';}
-		else if(v.equals("/")) {operator = '/';}
+		
+		// 1. 계산하는 입력
+		// 경우의 확인
+		for(i=0;i<listOp.length;i++)
+			if(v.equals(String.valueOf(listOp[i])) ){
+				isCalOp=true;
+				curOp = listOp[i];
+				break;
+			}
+		if(isCalOp) {
+			/* 연산자를 지속해서 입력한 경우를 예외로 처리 */
+			switch(state) {
+			case OP:op = curOp;
+				break;
+			/* 일반적인 경우 */
+			default:
+				//이전에 입력받은 연산자가 없다면
+				if(op == 0) {
+					op = curOp;
+					output=input;
+					input = 0;
+				}
+				else {
+					
+					
+				}
+				break;
+			}
+		}
 		else if(v.equals("=")) {
-			output = mid;
 			// 포맷적용
+			if(op != 0)
+				performOp(op);
 			print = formatter.format(output);
 			return;
 		}
-		else if(v.equals("←")) {input = input/10;}
-		else if(v.equals("clear")) {}
-		/* Number */
+		// 2. 기타 연산자
+		// Backspace
+		else if(v.equals("←")) {
+			
+			}
+		else if(v.equals("clear")){
+			input=0;
+			output=0;			
+		}
+		// 소수 입력의 시작
+		else if(v.equals(".")) {}
+		/* 3. 숫자입력
+		 * 소수입력을 고려해야한다
+		 */
 		else {
-			if(input == 0)input = Integer.valueOf(v);
-			else
-				input = input*10 + Integer.valueOf(v);
+			state = State.NUM;
+		//최대 자릿수는 E12
+		  if(input > 1000000000000f )return;
+		  
+		  if(input == 0)input = Integer.valueOf(v);
+		  else	input = input*10 + Integer.valueOf(v);
 		}
 		print = formatter.format(input);
 	}
 	public String getPrint() {
 		return print;
+	}
+	
+	// 계산하는 연산자를 수행하는 메서드
+	void performOp(char op) {
+		switch(op) {
+		case '+': output = output + input;
+			break;
+		case '-': output = output - input;
+			break;
+		case '*': output = output * input;
+			break;
+		case '/': output = output / input;
+			break;
+		}
+		
 	}
 }
--- week_01/src/week_03/MyFrame.java
+++ week_01/src/week_03/MyFrame.java
@@ -15,7 +15,12 @@ public class MyFrame extends JFrame  implements KeyListener,ActionListener{
 	JLabel t1;  
 	JPanel p1,p2,p3;
 	
-	String key[] = {"0","1","2","3","4","5","6","7","8","9","+","-","/","*","=","Backspace","Period"};
+	String key[] = {
+			"0","1","2","3","4","5","6","7","8","9",
+			"NumPad-0","NumPad-1","NumPad-2","NumPad-3","NumPad-4",
+			"NumPad-5","NumPad-6","NumPad-7","NumPad-8","NumPad-9",
+			"NumPad .","NumPad *","NumPad /","NumPad +","NumPad -",
+			"+","-","/","*","=","Backspace","Enter","Period"};
 	int NumButton = 16;
 	Calculator cal;
 	MyFrame(String title){
@@ -30,7 +35,22 @@ public class MyFrame extends JFrame  implements KeyListener,ActionListener{
 		p1 = new JPanel();
 		p2 = new JPanel();
 		p3 = new JPanel();
-		
+		/*
+		 * JFrame을
+		 * ----
+		 * |t1|
+		 * ----
+		 * |p2|
+		 * ----
+		 * |p3|
+		 * ----
+		 * 
+		 * 3등분한다
+		 * 
+		 * 각각의 p2,p3 (p1은 쓰지 않는다, t1만 사용하기 때문에 차이가 없다)
+		 * 는 2x5로 10개의 버튼을 가진다
+		 * 
+		 * */
 		setLayout(new GridLayout(3,1));
 		p2.setLayout(new GridLayout(2,5));
 		p3.setLayout(new GridLayout(2,5));
@@ -105,13 +125,13 @@ public class MyFrame extends JFrame  implements KeyListener,ActionListener{
 
 	@Override
 	public void keyPressed(KeyEvent e) {
+		//입력받은 keyEvent의 keyCode를 그에 대응하는 keyText로 변환
 		String keyInput = e.getKeyText(e.getKeyCode());
 		System.out.println("keyText : "+e.getKeyText(e.getKeyCode()));
-	
+		
 		boolean inCal=false;
-		/* 상용하지 않는 키 입려을 걸러준다*/
+		/* 사용하지 않는 키 입력을 걸러준다*/
 		for(int i=0;i<key.length;i++) {
-			
 			if(keyInput.equals(key[i]))inCal = true;
 		}
 		if(inCal) {
@@ -120,6 +140,11 @@ public class MyFrame extends JFrame  implements KeyListener,ActionListener{
 			keyInput = "←";
 		else if(keyInput.equals("Period"))
 			keyInput = ".";
+		else if(keyInput.equals("Enter"))
+			keyInput = "=";
+		// 키패드 숫자입력의 구분
+		if(keyInput.length()>2)
+			keyInput = keyInput.substring(7);
 		cal.input(keyInput);
 		t1.setText(cal.getPrint());
 		}
